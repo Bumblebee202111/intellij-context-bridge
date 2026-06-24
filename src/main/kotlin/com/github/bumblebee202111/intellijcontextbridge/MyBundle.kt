bdb5b21123a56438a48fdb3999a332a98dd3f07e
GitHub Action <action@github.com>
2026-06-24 17:14:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/bumblebee202111/intellijcontextbridge/MyBundle.kt
+++ src/main/kotlin/com/github/bumblebee202111/intellijcontextbridge/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bumblebee202111.intellijcontextbridge
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/bumblebee202111/intellijcontextbridge/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/bumblebee202111/intellijcontextbridge/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.bumblebee202111.intellijcontextbridge.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
